ab03e1cd4e09e9e2ceb2bac9e7f0024fa159b64a
이성준 <tjdwns54522@gmail.com>
2022-08-12 11:08:27 +0900

    매칭생성 클릭 시 match 테이블에 매칭 정보 insert, 매칭 list 화면에서 제목 클릭시 매칭 상세보기 화면 연동 데이터 뿌려주기 -lsj-

--- src/main/java/practice/myproject/controller/MatchController.java
+++ src/main/java/practice/myproject/controller/MatchController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import practice.myproject.domain.Match;
 import practice.myproject.domain.MatchDto;
 import practice.myproject.repository.MatchRepository;
 import practice.myproject.service.MatchService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +23,9 @@ public class MatchController {
 
     @GetMapping("/match/list")
     public ModelAndView matchList(ModelAndView mv) {
+        List<Match> matchList = matchRepository.findAll();
         mv.setViewName("match/list");
+        mv.addObject("match", matchList);
         return mv;
     }
 
@@ -32,8 +38,26 @@ public class MatchController {
 
     @PostMapping("/match/create")
     public ModelAndView createMatch(ModelAndView mv, MatchDto matchDto) {
+        System.out.println("matchDto = " + matchDto.getMatchTime());
+        System.out.println("matchDto = " + matchDto.getTitle());
+        System.out.println("matchDto = " + matchDto.getNotice());
+        System.out.println("matchDto = " + matchDto.getMatchAddress());
+        System.out.println("matchDto = " + matchDto.getLimitedPeople());
+        matchService.save(matchDto);
+
 
         mv.setViewName("redirect:/match/list");
+        return mv;
+    }
+
+    @GetMapping("/match/detail")
+    public ModelAndView matchDetail(ModelAndView mv, Long id) {
+        Optional<Match> findMatch = matchRepository.findById(id);
+        if(findMatch.isPresent()) {
+            mv.addObject("matchOne", findMatch.get());
+        }
+        mv.setViewName("match/detail");
+
         return mv;
     }
 
--- src/main/java/practice/myproject/service/MatchService.java
+++ src/main/java/practice/myproject/service/MatchService.java
@@ -2,11 +2,27 @@ package practice.myproject.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import practice.myproject.domain.Match;
+import practice.myproject.domain.MatchDto;
 import practice.myproject.repository.MatchRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class MatchService {
 
     private final MatchRepository matchRepository;
+
+    public void save(MatchDto matchDto) {
+        Match match = Match.builder()
+                .title(matchDto.getTitle())
+                .matchAddress(matchDto.getMatchAddress())
+                .limitedPeople(matchDto.getLimitedPeople())
+                .matchTime(matchDto.getMatchTime())
+                .notice(matchDto.getNotice())
+                .createTime(LocalDateTime.now())
+                .build();
+        matchRepository.save(match);
+    }
 }

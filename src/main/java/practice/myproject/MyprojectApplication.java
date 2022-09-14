743bc3b0f1735fb63e8f995a7512df82b214a9b9
이성준 <tjdwns54522@gmail.com>
2022-09-14 18:49:37 +0900

    putmapping, deletemapping 사용위해 bean등록  -lsj-

--- src/main/java/practice/myproject/MyprojectApplication.java
+++ src/main/java/practice/myproject/MyprojectApplication.java
@@ -2,12 +2,19 @@ package practice.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class MyprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyprojectApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
 	}
 
 }

3c3d0b44b2ce31ee954272b76fe2d3f86b941ebd
이성준 <tjdwns54522@gmail.com>
2022-09-19 12:02:17 +0900

    matchTime 컬럼 자료형 수정  -lsj-

--- src/main/java/practice/myproject/domain/MatchDto.java
+++ src/main/java/practice/myproject/domain/MatchDto.java
@@ -9,6 +9,6 @@ public class MatchDto {
 
     private int limitedPeople;  //제한 인원
     private String matchAddress;   //매칭 장소
-    private String matchTime;     //매칭 시간
+    private LocalDateTime matchTime;     //매칭 시간
     private String notice;  //공지사항, 한마디
 }

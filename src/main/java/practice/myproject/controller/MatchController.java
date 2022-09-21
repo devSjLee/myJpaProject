4ba3ad469070d83182c5df8ab246a720169ad9bf
이성준 <tjdwns54522@gmail.com>
2022-09-21 16:41:21 +0900

    match list 페이지에 날짜 넘기기  -lsj-

--- src/main/java/practice/myproject/controller/MatchController.java
+++ src/main/java/practice/myproject/controller/MatchController.java
@@ -18,6 +18,7 @@ import practice.myproject.service.MatchService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,11 +32,24 @@ public class MatchController {
 
 
     @GetMapping("/match/list")
-    public ModelAndView matchList(ModelAndView mv, String message) {
+    public ModelAndView matchList(ModelAndView mv, String message, String dateKey) {
+        LocalDateTime now = LocalDateTime.now();
+        ArrayList<Object> dateArr = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            dateArr.add(now.plusDays(i));
+        }
         Pageable pageable = PageRequest.of(0, 10, Sort.by("matchTime").ascending());
-        Page<Match> matchList = matchRepository.findAll(pageable);
-        mv.addObject("matchList", matchList);
+
+        if (dateKey != null) {
+            List<Match> matchList = matchService.findMatchList(dateKey, pageable);
+            mv.addObject("matchList", matchList);
+        } else {
+            Page<Match> matchList = matchRepository.findAll(pageable);
+            mv.addObject("matchList", matchList);
+        }
+
         mv.addObject("message", message);
+        mv.addObject("dateArr", dateArr);
         mv.setViewName("match/list");
         return mv;
     }
@@ -52,7 +66,7 @@ public class MatchController {
         Match match = matchService.save(matchDto, loginId);
 
         //매칭만든사람 자동으로 해당 매칭에 가입
-        System.out.println("이게뭐지: "+loginId);
+        System.out.println("이게뭐지: " + loginId);
         matchService.matchAttend(loginId, match.getId());
 
         mv.setViewName("redirect:/match/list");
@@ -62,7 +76,7 @@ public class MatchController {
     @GetMapping("/match/detail")
     public ModelAndView matchDetail(ModelAndView mv, Long id, String loginId, String message) {
 
-        if(id != null) {
+        if (id != null) {
             Optional<Match> findMatch = matchRepository.findById(id);
             if (findMatch.isPresent()) {
                 mv.addObject("matchOne", findMatch.get());
@@ -89,10 +103,10 @@ public class MatchController {
         redirectAttributes.addAttribute("loginId", loginId);
 
         Optional<Match> match = matchRepository.findById(id);
-        if(match.isPresent()) {
+        if (match.isPresent()) {
             Match findMatch = match.get();
             for (Member member : findMatch.getMembers()) {
-                if(member.getLoginId().equals(loginId)) {
+                if (member.getLoginId().equals(loginId)) {
                     redirectAttributes.addAttribute("message", "이미 가입중인 매칭입니다.");
                     mv.setViewName("redirect:/match/detail");
                     return mv;
@@ -113,18 +127,19 @@ public class MatchController {
     public ModelAndView updateMatchForm(ModelAndView mv, @PathVariable("id") Long id) {
 
         Optional<Match> match = matchRepository.findById(id);
-        System.out.println("아이디: "+id);
-        System.out.println("아이디: "+ match.get().getNotice());
+        System.out.println("아이디: " + id);
+        System.out.println("아이디: " + match.get().getNotice());
         mv.addObject("matchOne", match.get());
         mv.addObject("matchDto", new MatchDto());
         mv.setViewName("match/updateMatchForm");
         return mv;
     }
+
     @PutMapping("/match/{id}")
     public ModelAndView updateMatch(ModelAndView mv, @PathVariable("id") Long id, MatchDto matchDto, String loginId, RedirectAttributes redirectAttributes) {
 
         Long result = matchService.updateMatch(id, matchDto);
-        if(result != null) {
+        if (result != null) {
             redirectAttributes.addAttribute("id", id);
             redirectAttributes.addAttribute("loginId", loginId);
             redirectAttributes.addAttribute("message", "수정완료!!");
@@ -137,7 +152,7 @@ public class MatchController {
     public ModelAndView deleteMatch(ModelAndView mv, @PathVariable Long id, RedirectAttributes redirectAttributes) {
 
         Long aLong = matchService.deleteMatch(id);
-        System.out.println("뭐지: "+aLong);
+        System.out.println("뭐지: " + aLong);
         redirectAttributes.addAttribute("message", "삭제 완료!!");
         mv.setViewName("redirect:/match/list");
         return mv;

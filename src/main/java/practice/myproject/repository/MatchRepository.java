0264f6565af67d7811d20b0953ba414ec03ba3d6
이성준 <tjdwns54522@gmail.com>
2022-10-04 17:25:56 +0900

    오늘날짜기준 매칭시간이 지난 매칭들 조회하는 쿼리 추가  -lsj-

--- src/main/java/practice/myproject/repository/MatchRepository.java
+++ src/main/java/practice/myproject/repository/MatchRepository.java
@@ -8,9 +8,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import practice.myproject.domain.Match;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
     @Query("select m from Match m where m.matchTime like %:dateKey%")
     Page<Match> findMatchList(@Param("dateKey") String dateKey, Pageable pageable);
+
+    @Query("select m from Match m where FORMATDATETIME(m.matchTime, 'yyyy-MM-dd') < FORMATDATETIME(NOW(), 'yyyy-MM-dd')")
+    List<Match> findDeleteMatch();
 }

829b1fd046ed70b5d3c26671dd1247fab6b03b57
이성준 <tjdwns54522@gmail.com>
2022-10-12 17:19:54 +0900

    dto 프로퍼티 변경  -lsj-

--- src/main/java/practice/myproject/domain/MatchDto.java
+++ src/main/java/practice/myproject/domain/MatchDto.java
@@ -7,8 +7,19 @@ import java.time.LocalDateTime;
 @Data
 public class MatchDto {
 
+
     private int limitedPeople;  //제한 인원
-    private Long groundId;   //매칭 장소
     private String matchTime;     //매칭 시간
     private String notice;  //공지사항, 한마디
+
+    private Double x;   //x좌표
+    private Double y;   //y좌표
+
+    private String address;     //매칭장소
+    private String groundName;      //구장명
+    private String callNumber;      //연락처
+    private String showerYn;   //샤워시설 여부
+    private String sportsWearYn;   //운동복대여 여부
+    private String shoesYn;   //풋살화대여 여부
+    private String parkingYn;   //주차가능 여부
 }
